a84950c58cbcd9def6fc3c338344aa54d6623496
alimmst <79074714+alimmst@users.noreply.github.com>
2021-04-12 11:00:41 +0430

    added-migrations

---
+++ Library/Library.Migrations/Migrations/_202104101842_CreatedTables.cs
@@ -0,0 +1,49 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Migrations.Migrations
+{
+    [Migration(202104101842)]
+    public class _202104101842_CreatedTables : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("BookCategories")
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("Title").AsString().NotNullable();
+
+            Create.Table("Books")
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("Name").AsString().NotNullable()
+                .WithColumn("Author").AsString().NotNullable()
+                .WithColumn("AgeGroup").AsInt32().NotNullable()
+                .WithColumn("BookCategoryId").AsInt32().NotNullable()
+                    .ForeignKey("FK_Books_BookCategories", "BookCategories", "Id");
+
+            Create.Table("Members")
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("FullName").AsString().NotNullable()
+                .WithColumn("Address").AsString().NotNullable()
+                .WithColumn("Age").AsInt32().NotNullable();
+
+            Create.Table("Loans")
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("BookId").AsInt32().NotNullable()
+                    .ForeignKey("FK_Loans_Books", "Books", "Id")
+                .WithColumn("MemeberId").AsInt32().NotNullable()
+                    .ForeignKey("FK_Loans_Members", "Members", "Id");
+
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Loans");
+            Delete.Table("Members");
+            Delete.Table("Books");
+            Delete.Table("BookCategories");
+        }
+
+    }
+}
---
+++ Library/Library.Migrations/Runner.cs
@@ -0,0 +1,38 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Library.Migrations
+{
+    class Runner
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = CreateServices();
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        private static IServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddSqlServer()
+                    .WithGlobalConnectionString("Server=.;Database=Library;Trusted_Connection=True;")
+                    .ScanIn(typeof(Runner).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+            runner.MigrateUp();
+        }
+    }
+}
